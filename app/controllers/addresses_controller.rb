3e8b16405d90c26190ecbdf7d883945baf63beb6
yamaday0u <35hallelujah00@gmail.com>
2021-01-16 23:32:55 +0900

    Create views for address

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,4 +1,19 @@
 class AddressesController < ApplicationController
   def index
+    @addresses = Address.all
+  end
+
+  def new
+    @address = Address.new
+  end
+
+  def create
+    @address = Address.new(address_params)
+    if @address.valid?
+      @address.save
+      redirect_to :index
+    else
+      render :new
+    end
   end
 end
