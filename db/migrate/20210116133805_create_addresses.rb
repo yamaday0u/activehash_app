6726d90ece5d60148a0108a089788b12256af82f
yamaday0u <35hallelujah00@gmail.com>
2021-01-16 22:47:19 +0900

    Create address model & address table

---
+++ db/migrate/20210116133805_create_addresses.rb
@@ -0,0 +1,9 @@
+class CreateAddresses < ActiveRecord::Migration[6.0]
+  def change
+    create_table :addresses do |t|
+      t.string :name, null: false
+      t.integer :prefecture_id, null: false
+      t.timestamps
+    end
+  end
+end
